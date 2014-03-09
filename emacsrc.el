46a05dd490e6e5434df9db582d48ff58ec8b1094
Winter Fu <winterfu@gmail.com>
2014-03-09 11:35:04 +0800

    Add 3 sources to package manager.

---
+++ emacsrc.el
@@ -0,0 +1,7 @@
+;; Emacs package manager, Add source into package-archives
+(require 'package)
+(dolist (source '(("marmalade" . "http://marmalade-repo.org/packages/")
+                  ("elpa" . "http://tromey.com/elpa/")
+                  ("melpa" . "http://melpa.milkbox.net/packages/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
